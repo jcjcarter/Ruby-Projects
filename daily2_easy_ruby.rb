e3c851ad1d5215da042013002c25aad11350b31e
jcjcarter <Ace@Jaysons-MBP.attlocal.net>
2015-10-24 21:57:20 -0500

    Initial.

---
+++ daily2_easy_ruby.rb
@@ -0,0 +1,11 @@
+class Daily2EasyRuby
+
+
+  puts "Please enter your name:"
+  name = gets.chomp
+  puts "Please enter your age:"
+  age = gets.chomp
+  puts "Please enter your reddit username:"
+  rddtusr = gets.chomp
+  puts "Your name is #{name}, you are #{age} years old, and your username is #{rddtusr}"
+end
